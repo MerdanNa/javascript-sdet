e6682c956de2c39b21f4fa795475aa7580dbe4d5
merdannazarov <merdannazarov@192.168.0.3>
2018-11-21 13:28:15 -0500

    JavaProject1

---
+++ 1-My First Program/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Salam Dunya!");
+          System.out.println("Allaha sukur Elhamdulillah");
+          System.out.println("Allahu Akbar");
+          System.out.println("- Asalamu aleykum  gowymyn ");
+          System.out.println("-Aleykum salam ,sukur gowy");
+
+
+	}
+
+}
